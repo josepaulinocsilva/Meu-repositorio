26117aadb033ad78e7b10d962d481af4c021260b
josepaulinocsilva <jpaulino57@hotmail.com>
2026-06-05 16:50:42 -0300

    Versão final do linguagem.js 100% corrigida e testada

--- linguagem.js
+++ linguagem.js
@@ -1,8 +1,13 @@
 function linguagemDosGatosECachorros(animal) {
+  // mostrar que entrou na função:
+  console.log("Estudando Git e GitHub");
+
   if (animal == "cachorro") return "au au";
   else if (animal == "gato") return "miau";
   else if (animal == "vaca") return "muu";
   else return "Informe gato ou cachorro ou vaca";
-
-  console.log("Estudando Git e GitHub");
 }
+// aqui vamos imprimir o resultado dasa 3 chamadas:
+console.log(linguagemDosGatosECachorros("cachorro"));
+console.log(linguagemDosGatosECachorros("gato"));
+console.log(linguagemDosGatosECachorros());
