836f81d2f2d5c69daa53f79528fc1a5fe42bbf1d
josepaulinocsilva <jpaulino57@hotmail.com>
2026-06-05 16:03:12 -0300

    Ajustes e testes locais no linguagem.js

--- linguagem.js
+++ linguagem.js
@@ -1,8 +1,14 @@
 function linguagemDosGatosECachorros(animal) {
+  // será executado antes do return.
+  console.log("Estudando Git e GitHub");
+
   if (animal == "cachorro") return "au au";
   else if (animal == "gato") return "miau";
   else if (animal == "vaca") return "muu";
   else return "Informe gato ou cachorro ou vaca";
-
-  console.log("Estudando Git e GitHub");
 }
+
+// Chamamos a função e mandamos imprimir o resultado para:
+console.log(linguagemDosGatosECachorros("cachorro"));
+console.log(linguagemDosGatosECachorros("gato"));
+console.log(linguagemDosGatosECachorros());
